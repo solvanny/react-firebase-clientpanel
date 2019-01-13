6103b7e936bbe9fc96d2a0435c0fbaa2028f68f4
Ion Soltan <ion.soltan@hotmail.com>
2019-01-13 16:48:35 -0500

    first commit

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <Link to ="/client/add" className="btn btn-success btn-block">
+      <i className="fas fa-plus" /> New
+    </Link>
+  )
+}
